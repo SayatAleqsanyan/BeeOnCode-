888acb2e277d2aadb2b6af5deaab2df9702cb478
SayatAleqsanyan <sayad.aleksanyan@mail.ru>
2024-11-01 23:05:40 +0400

    Js Lesson 1

---
+++ Lesson/Lesson 101/script.js
@@ -0,0 +1,36 @@
+console.log(555);
+console.log(typeof( 555 ));
+
+console.log("555");
+console.log(typeof("555"));
+
+console.log(3 > 4);
+console.log(typeof (3 > 4));
+
+//alert(111);
+//prompt("Name");
+
+/*
+let a;
+var b = 0;
+var b = 2;
+const C ="Const";
+console.log(a + b); 
+console.log(4 + 6 + "5" + 5);
+console.log(2 ** 8);
+*/
+
+let Names = prompt("greq jer anuny" );
+console.log("jer anuny: `" + Names + "` e");
+
+let Age = Number(prompt("greq jer tariqy" ));
+console.log("duq: `" + Age + "` eq");
+
+if (5 > 5) {
+  console.log(111);
+} else if (5 == 5) {
+  console.log(222);
+} else {
+  console.log(333);
+}
+
