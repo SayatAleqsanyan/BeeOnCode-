7074f0957865aaedecbd8c9bf3cf0aca94e43956
SayatAleqsanyan <sayad.aleksanyan@mail.ru>
2024-12-21 16:05:04 +0400

    00

---
+++ JS +/Lesson/Lesson 08.1/script.js
@@ -0,0 +1,21 @@
+const link = document.querySelector('a');   
+
+const singl = link.getAttribute('href'); 
+const multisingl = link.getAttributeNames();
+
+console.log(singl, multisingl); 
+
+
+link.setAttribute('href', 'https://chatgpt.com/');
+
+
+
+
+
+
+
+
+
+
+
+
